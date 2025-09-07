e029c2d030aed6d1a1ec2fe0604d2c03e1157e69
Michael Zhao <Michaelzhao4567@gmail.com>
2025-09-06 22:08:03 -0400

    Player movement and speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
 
-    private Rigidbody rb; 
+
     // Start is called before the first frame update
     void Start() {   
         rb = GetComponent<Rigidbody>();
@@ -15,10 +19,16 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
     }
 }
